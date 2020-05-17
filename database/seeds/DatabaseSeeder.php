f6121302ab028070ea4ee5bde906b17d8b5e37e9
suren <suren@owc.ourwebcreation.com>
2020-05-17 04:51:38 -0700

    create foreign key for role_user table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+
+        // supposed to only apply to a single connection and reset it's self
+		// but I like to explicitly undo what I've done for clarity
+		//DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
